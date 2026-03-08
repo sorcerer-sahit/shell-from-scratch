5b272775de8e6036914758343ded355884b39ff1
sorcerer-sahit <sahithp54@gmail.com>
2026-03-08 18:24:18 +0530

    print $ sign

--- src/main/java/com/sahit/Main.java
+++ src/main/java/com/sahit/Main.java
@@ -4,6 +4,6 @@ package com.sahit;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("$");
     }
 }
