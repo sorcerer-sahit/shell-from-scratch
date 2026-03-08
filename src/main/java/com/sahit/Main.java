9e6ec7c3f8ea4128dd1e041e53ecfbc6a7f6ee67
sorcerer-sahit <sahithp54@gmail.com>
2026-03-08 21:08:55 +0530

    added echo functionality

--- src/main/java/com/sahit/Main.java
+++ src/main/java/com/sahit/Main.java
@@ -9,8 +9,16 @@ public class Main {
             System.out.print("$ ");
 
             String userInput = scanner.nextLine();
-
-            System.out.println(userInput + ": command not found");
+            if(userInput.equals("exit")) return;
+            String[] words = userInput.split(" ");
+            if(words[0].equals("echo")) {
+                for(int i = 1;i < words.length;i++) {
+                    System.out.print(words[i]);
+                    if(i != words.length - 1) System.out.print(" ");
+                    else System.out.println();
+                }
+            }
+            else System.out.println(userInput + ": command not found");
         }
     }
 }
